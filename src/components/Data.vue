5d8df16f4d68591c8b2dcce2c01b950689db5b5c
dunglv1607 <levandung1607@gmail.com>
2020-07-23 09:49:30 +0700

    push

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,93 +1,143 @@
 <template>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">STT</th>
-      <th scope="col">Số Người</th>
-      <th scope="col">Buổi</th>
-      <th scope="col">Nơi Tổ Chức</th>
-      <th scope="col">Tên Khách</th>
-      <th scope="col">Giới Tính</th>
-      <th scope="col">Ngày</th>
-      <th scope="col">Món</th>
-      <th scope="col">Yêu Cầu</th>
-      <th scope="col">Qua</th>
-      <th scope="col">Tổng Tiền</th>
-      <th scope="col">hành động</th>
-    </tr>
-  </thead>
-  <tbody v-if="demo" demo=true> 
-    <tr v-for="(item, index) in data" :key="index">
-      <th scope="row">{{item.id}}</th>
-      <td>{{item.amount}}</td>
-      <td>{{item.session}}</td>
-      <td>{{item.place}}</td>
-      <td>{{item.name}}</td>
-      <td>{{item.sex}}</td>
-      <td>{{item.date}}</td>
-      <td>{{item.choose}}</td>
-      <td>{{item.request}}</td>
-      <td>{{item.source}}</td>
-      <td>{{item.totalPrice}}</td>
-      <td>
-        <button type="button" class="btn btn-primary" @click="edit(item)">Sửa</button>
-        <button type="button" class="btn btn-danger" @click="deleteItem(item)">xóa</button>
-      </td>
-    </tr>  
-  </tbody>
-  <tbody v-else demo=false> 
-    <tr v-for="(item, index) in data" :key="index">
-      <th scope="row">{{item.id}}</th>
-      <td><input type="text" v-model="item.amount"></td>
-      <td><input type="text" v-model="item.session"></td>
-      <td><input type="text" v-model="item.place"></td>
-      <td><input type="text" v-model="item.name"></td>
-      <td><input type="text" v-model="item.sex"></td>
-      <td><input type="date" v-model="item.date"></td>
-      <td><input type="text" v-model="item.choose"></td>
-      <td><input type="text" v-model="item.request"></td>
-      <td><input type="text" v-model="item.source"></td>
-      <td>{{item.totalPrice}}</td>
-      <td>
-        <button type="button" class="btn btn-primary" @click="save(item)">Save</button>
-        <button type="button" class="btn btn-danger" @click="deleteItem(item)">xóa</button>
-      </td>
-    </tr>
-  </tbody>
-</table>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">STT</th>
+        <th scope="col">Số Người</th>
+        <th scope="col">Buổi</th>
+        <th scope="col">Nơi Tổ Chức</th>
+        <th scope="col">Tên Khách</th>
+        <th scope="col">Giới Tính</th>
+        <th scope="col">Ngày</th>
+        <th scope="col">Món</th>
+        <th scope="col">Yêu Cầu</th>
+        <th scope="col">Qua</th>
+        <th scope="col">Tổng Tiền</th>
+        <th scope="col">hành động</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <th scope="row">{{item.id}}</th>
+        <td>{{item.amount}}</td>
+        <td>{{item.session}}</td>
+        <td>{{item.place}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.sex}}</td>
+        <td>{{item.date}}</td>
+        <td>{{item.choose}}</td>
+        <td>{{item.request}}</td>
+        <td>{{item.source}}</td>
+        <td>{{item.totalPrice}}</td>
+        <td>
+           <button
+            @click="edit(item)"
+            type="button"
+            class="btn btn-primary"
+            data-toggle="modal"
+            data-target="#exampleModal"
+            data-whatever="@mdo"
+          >Sửa</button>
+
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Sửa Thông Tin</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                   <form>
+                    <div class="form-group">
+                      <label for="recipient-name" class="col-form-label"  >Số Người:</label>
+                      <input v-model="item.amount" type="number" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Buổi:</label>
+                      <input v-model="item.session" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Nơi tổ chức:</label>
+                      <input v-model="item.place" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Tên khách:</label>
+                      <input v-model="item.name" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Giới tính:</label>
+                      <input v-model="item.sex" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Ngày:</label>
+                      <input v-model="item.date" type="date" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Món:</label>
+                      <input v-model="item.choose" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Yêu cầu:</label>
+                      <input v-model="item.request" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                     <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Qua:</label>
+                      <input v-model="item.sourcesss" type="text" class="form-control" id="recipient-name" />
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button @click="save" type="button" class="btn btn-primary">Save</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button type="button" class="btn btn-danger" @click="deleteItem(item)">xóa</button>
+        </td>
+              
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            data:this.$store.state.data,
-            demo:true,
-            text:{
-                amount:"",  
-                name: "",
-                session: "",
-                selected: "",
-                data_text: "",
-                sex: "",
-                choose: "",
-                date: "",
-                request: "",
-                source: ""
-            }
-        }
-        },
-    methods: {
-        edit() {
-          this.demo=false
-        },
-        save(){
-          this.$store.dispatch('save',this.text)
-          this.demo=true
-        },
-        deleteItem(item) {
-          this.$store.dispatch('deleteItem', item);
-        }
+  data() {
+    return {
+      data: this.$store.state.data,
+      text: {
+        amount: "",
+        name: "",
+        session: "",
+        selected: "",
+        data_text: "",
+        sex: "",
+        choose: "",
+        date: "",
+        request: "",
+        source: ""
+      }
+    };
+  },
+  methods: {
+    edit(item) {
+      console.log(item.id);
+    },
+    save(item) {
+      console.log(item);
+      this.$store.dispatch("save", this.text);
+    },
+    deleteItem(item) {
+      this.$store.dispatch("deleteItem", item);
     }
-}
-
+  }
+};
 </script>
